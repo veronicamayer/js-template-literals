82809de6087e6ca882cf0fa8eabf71c86a25870b
veronicamayer <mayerveronica.vm@gmail.com>
2023-01-24 15:00:19 +0100

    shorter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,9 @@
 let name = "Veronica";
 let nachname = "Mayer";
 let alter = 25;
-let geburtsort = "Waldkirch";
-let größe = 170;
-let gewicht = 56;
 const hobbies = ["Squash", "Amnesty", "Surfen", "Fotografie"];
 let hobbiesString = hobbies.join(", ");
-const computerspiele = ["Mario Party", "Zelda"];
-let computerspieleString = computerspiele.join(", ");
 let lieblingsessen = "Ramen";
-let lieblingssport = "Turnen";
-let lieblingsmodemarke = "Armed Angels";
-let lieblingsjahreszeit = "Sommer";
-let lieblingsurlaubsort = "New York";
 
 let concat1 = "Mein Name ist " + name + " " + nachname + ". Ich bin " + alter + " Jahre alt."; 
 let concat2 = `Meine Hobbies sind ${hobbiesString}, und am liebsten esse ich ${lieblingsessen}.`;
